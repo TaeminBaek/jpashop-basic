ee7edc88ef8697615b5ea0cedec00ad90d9e6041
Taemin Baek <0221ptm@naver.com>
2022-06-22 17:39:02 +0900

    연관관계 관리

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -22,7 +22,7 @@ public class Category {
     @JoinColumn(name = "PARENT_ID")
     private Category parent;
 
-    @OneToMany(mappedBy = "parent")
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
     private List<Category> child = new ArrayList<>();
 
     public Long getId() {
